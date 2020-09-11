8f7465cd9521c224108146edebdf6ef1d77c011f
Stephane Treyssede <toto>
2020-09-11 15:15:29 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+html, body
+{
+    height: 100%;
+    margin:0;
+    padding:0;
+}
+
+.LogoImage {
+  position: absolute;
+  top: 50%;
+  left :50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}   
+.background-media__video{display:block;position:absolute;bottom:0;width:auto;min-width:100%;height:auto;min-height:100%}
+</style>
+</head>
+<body>
+                    <video autoplay="autoplay" muted="" loop="" class="background-media__video">
+                        <source src="video/624638333.mp4" type="video/mp4">
+						<!-- https://player.vimeo.com/external/183723365.hd.mp4?s=1a82fd98f6071cc8e17d613d554bcac8200ef063&profile_id=119&dnt=1 -->
+						
+                    </video>
+<div><img class="LogoImage" src="img/LysOffice-1.png"/></div>
+
+</body>
