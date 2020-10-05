bc03ee05e6bd1a4c37bd79369129ad371bcd1d5e
Stéphane Treyssède <stephanetreyssede@macbook-pro-de-galys.home>
2020-10-05 08:49:32 +0200

    202010050849

---
+++ js/main.js
@@ -0,0 +1,18 @@
+window.onscroll = function() {stickymenu()};
+
+var sticky = 52;
+function stickymenu() {
+  if (window.pageYOffset >= sticky) {
+    
+    $('.scrollToTop').addClass("top-on").removeClass("top-off");
+  } else {
+      
+    $('.scrollToTop').addClass("top-off").removeClass("top-on");
+  }
+}
+
+$(function() {
+    $(".scrollToTop").click(function() {
+        $("body").animate({scrollTop : 0}, 500);
+    });
+});
